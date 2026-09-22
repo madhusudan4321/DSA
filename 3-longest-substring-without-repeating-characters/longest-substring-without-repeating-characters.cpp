a1742d168af3e8a6152e02674e01508f968bee5d
Madhusudan Yadav <132561628+madhusudan4321@users.noreply.github.com>
2026-09-22 20:48:54 +0530

    Time: 36 ms (63.31%) | Memory: 19.4 MB (43.41%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,8 +4,8 @@ public:
         int count = 0;
         int max1 = INT_MIN;
         int left = 0;
-        if(s.size()==0) return 0;
         unordered_map<char,int>mp;
+        if(s.size()==0) return 0;
         for(int i=0;i<s.size();i++){
             if(mp.find(s[i])==mp.end()){
                 mp[s[i]]=i;
@@ -14,6 +14,7 @@ public:
                 left = max(left,mp[s[i]]);
                 mp[s[i]]=i;
                 count = i-left;
+                
             }
             max1 = max(max1,count);
         }
